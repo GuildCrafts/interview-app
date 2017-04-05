ce570ade11e0f93e6a0a255f060746e2c96ad2ec
lizzkats <katsnelson.lizz@gmail.com>
2017-04-04 17:42:44 -0700

    finished testing

---
+++ src/database/migrations/20170403160313_tables.js
@@ -0,0 +1,29 @@
+exports.up = function(knex, Promise) {
+
+  return Promise.all([
+
+    knex.schema.createTable('users', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('github');
+    }),
+
+    knex.schema.createTable('questions', function(table) {
+      table.increments('id').primary();
+      table.string('tags');
+      table.string('question');
+      table.string('difficulty');
+      table.string('topic');
+      table.string('answer');
+    })
+  ])
+
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('questions'),
+  ])
+
+};
