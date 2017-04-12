2b54c87497f41202ae62d44c7802616bb9e163a3
HJBowers <HJBowers@gmail.com>
2017-04-12 11:42:53 -0700

    fixed a rebase typo

--- src/server/routes/questions.js
+++ src/server/routes/questions.js
@@ -4,15 +4,10 @@ import * as utilities from '../../database/queries/utilities.js'
 
 const router = express.Router()
 
-<<<<<<< HEAD
-
 router.get('/', (request, response) => {
   const {difficulty, topics} = request.params
-  console.log('request.params >>>>', request.params)
   utilities.findAll('questions')
   .then( questions => {
-    console.log('questions >>>>', questions)
-
       if(difficulty !== 'any') {
         questions = questions.filter( questions => !questions.difficulty )
       }
@@ -21,37 +16,7 @@ router.get('/', (request, response) => {
       }
       response.json(questions)
   })
-=======
-// router.get('/', (request, response) => {
-//   const {difficulty, tags} = request.params
-//   utilities.findAllWhere('questions', request.params, input)
-//   .then( results => response.json( results ) )
-//   .catch( err => console.log('err', err) )
-// })
-
-//tags is a string, we will probably need to split it?
-router.get('/', (request, response) => {
-  const {difficulty, tags} = request.params
-  utilities.findAll('questions')
-  .then( questions => {
-    console.log('questions >>>>', questions)
-    questions.filter( () => {
-      if(request.params.difficulty === questions.difficulty) {
-        console.log('yes it is difficult');
-      }
-      if(tags) {
-        console.log('there are tags');
-      }
-      if(!difficulty && !tags){
-        return
-      }
-      // questions => response.json(questions)
-    }
-  )
 })
->>>>>>> bb6c5d289a6ae3ad2f5b8fecdb1f5632362012da
-})
-
 
 router.post('/questions', (request, response) => {
   const { attributes } = request.body
