85a5bbb8f50af8e868c192b923a1ac3bc6b8dd84
Breyana Scales <breyana@Breyanas-MacBook-Pro.local>
2017-04-24 15:28:05 -0700

    setting up interview files

---
+++ src/server/routes/interview.js
@@ -0,0 +1,5 @@
+import express from 'express'
+import * as questions from '../../database/queries/questions.js'
+import * as utilities from '../../database/queries/utilities.js'
+
+router.wtfarewegoingtowriteforroutes('/')
