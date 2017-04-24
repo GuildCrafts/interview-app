36402ee679db69e461ad31d866926d62e532624b
Breyana Scales <breyana@Breyanas-MacBook-Pro.local>
2017-04-24 15:31:45 -0700

    deleted the line for is_completed

--- src/database/migrations/20170424134531_add-interviews-table.js
+++ src/database/migrations/20170424134531_add-interviews-table.js
@@ -3,7 +3,6 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable('interviews', function(table) {
       table.integer('user_id')
       table.foreign('user_id').references('users.id')
-      table.boolean('is_completed');
       table.text('feedback');
     }),
   ])
