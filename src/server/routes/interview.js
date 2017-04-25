1ba2949a89b13ea5f7f964ce2e035a57daaf717c
Breyana Scales <breyana@Breyanas-MacBook-Pro.local>
2017-04-25 16:56:33 -0700

    interview queries and tests

--- src/server/routes/interview.js
+++
@@ -1,5 +0,0 @@
-import express from 'express'
-import * as questions from '../../database/queries/questions.js'
-import * as utilities from '../../database/queries/utilities.js'
-
-router.wtfarewegoingtowriteforroutes('/')
