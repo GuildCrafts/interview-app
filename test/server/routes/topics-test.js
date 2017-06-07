a694635129dcecec617920911d47a3a2d121d917
leikkisa <sallymaki@gmail.com>
2017-06-07 15:54:36 -0700

    Finish topic route test, except error handling

--- test/server/routes/topics-test.js
+++ test/server/routes/topics-test.js
@@ -20,13 +20,46 @@ describe.only('api/topics', () => {
 
   it('Should get all topics with an approved question associated', (done) => {
     chai.request(app)
-    .get('/api/topics/with-questions')
+    .post('/api/questions/')
+    .send({
+      question: 'Who is Murphy?',
+      answer: 'your mom',
+      topics: 'core-javascript',
+      level: 'beginner',
+      points: 1,
+    })
+    //read the question
+    .then((newQuestionRes) => {
+      newQuestionRes.should.have.status(200);
+      let newQuestionId = newQuestionRes.body
+      return chai.request(app)
+      .get('/api/questions/' + newQuestionId )
+    })
+    //update the question
+    .then(newQuestion => {
+      return chai.request(app)
+      .put('/api/questions/approval/' + newQuestion.body.id)
+      .send({ id: newQuestion.body.id,
+        question: 'Who is Murphy?',
+        answer: 'your mom',
+        level: 'jedi',
+        hints: [],
+        game_mode: null,
+        points: 1,
+        topics: [ 'core-javascript' ],
+        is_approved: true
+      })
+    })
+    .then((editedQuestion) => {
+      return chai.request(app)
+      .get('/api/topics/with-questions')
       .then((res) => {
         res.should.have.status(200)
         res.body.should.be.a('array')
-        res.body.should.eql(['functional-programming'])
+        res.body.should.eql(['core-javascript'])
+        done()
       })
-      done();
+    })
   })
 
 })
